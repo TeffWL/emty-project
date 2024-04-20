90d53529122c4f1ffb784c4cde0e6a95b70a98f8
TeffWL <127594022+TeffWL@users.noreply.github.com>
2024-04-20 21:04:16 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+
+
+        WebElement input = driver.findElement(By.xpath("//*[@id='my-text-id']"));
+        input.click();
+        input.sendKeys("text");
+
+
+        WebElement input_password = driver.findElement(By.xpath("/html/body/main/div/form/div/div[1]/label[2]/input"));
+        input_password.sendKeys("password");
+        input_password.click();
+
+    }
+}
